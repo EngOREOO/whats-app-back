1b78e03d88890d55c9aa5123a0003e206e4f2b3e
Mohamed Soliman <prackels@gmail.com>
2025-08-13 05:36:17 +0300

    Cloud Run

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production" 
-    ? "https://api.codiaumtech.com/api"
+    ? "https://wh-front.codiaumtech.com/api"
     : process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 
 const api = axios.create({
